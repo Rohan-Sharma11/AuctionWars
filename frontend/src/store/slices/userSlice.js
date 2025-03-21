daa7737a315166e4d57a84c81cc4c1f636d8a266
Rohan Sharma <rohansh3535@gmail.com>
2025-03-21 15:52:50 +0530

    Updated backend code

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -91,7 +91,7 @@ export const register = (data) => async (dispatch) => {
   dispatch(userSlice.actions.registerRequest());
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/v1/user/register",
+      "http://3.110.157.235:5000/api/v1/user/register",
       data,
       {
         withCredentials: true,
@@ -117,7 +117,7 @@ export const login = (data) => async (dispatch) => {
   dispatch(userSlice.actions.loginRequest());
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/v1/user/login",
+      "http://3.110.157.235:5000/api/v1/user/login",
       data,
       {
         withCredentials: true,
@@ -137,7 +137,7 @@ export const login = (data) => async (dispatch) => {
 export const logout = () => async (dispatch) => {
   try {
     const response = await axios.get(
-      "http://localhost:5000/api/v1/user/logout",
+      "http://3.110.157.235:5000/api/v1/user/logout",
       { withCredentials: true }
     );
     dispatch(userSlice.actions.logoutSuccess());
@@ -153,7 +153,7 @@ export const logout = () => async (dispatch) => {
 export const fetchUser = () => async (dispatch) => {
   dispatch(userSlice.actions.fetchUserRequest());
   try {
-    const response = await axios.get("http://localhost:5000/api/v1/user/me", {
+    const response = await axios.get("http://3.110.157.235:5000/api/v1/user/me", {
       withCredentials: true,
     });
     dispatch(userSlice.actions.fetchUserSuccess(response.data.user));
@@ -169,7 +169,7 @@ export const fetchLeaderboard = () => async (dispatch) => {
   dispatch(userSlice.actions.fetchLeaderboardRequest());
   try {
     const response = await axios.get(
-      "http://localhost:5000/api/v1/user/leaderboard",
+      "http://3.110.157.235:5000/api/v1/user/leaderboard",
       {
         withCredentials: true,
       }
